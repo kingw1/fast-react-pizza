49f2a2e1a657d2e1bff8c41b9f55f6bdf239286c
King <wiwichai@gmail.com>
2024-10-15 16:39:38 +0700

    layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,41 @@
-export default function App() {
-  return <div>Hello Vite!</div>;
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AppLayout from "./ui/AppLayout";
+import Home from "./ui/Home";
+// import Menu from "./features/menu/Menu";
+// import Cart from "./features/cart/Cart";
+// import Order from "./features/order/Order";
+// import CreateOrder from "./features/order/CreateOrder";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      //   {
+      //     path: "/menu",
+      //     element: <Menu />,
+      //   },
+      //   {
+      //     path: "/cart",
+      //     element: <Cart />,
+      //   },
+      //   {
+      //     path: "/order/new",
+      //     element: <CreateOrder />,
+      //   },
+      //   {
+      //     path: "/order/:orderId",
+      //     element: <Order />,
+      //   },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
 }
+
+export default App;
