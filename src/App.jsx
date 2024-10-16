1306a21cc37c401d4615565ee6bbb67d24ca17bb
King <wiwichai@gmail.com>
2024-10-16 11:08:18 +0700

    create order action

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ import Home from "./ui/Home";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import Order, { loader as orderLoader } from "./features/order/Order";
-// import CreateOrder from "./features/order/CreateOrder";
+import CreateOrder, {
+  action as createOrderAction,
+} from "./features/order/CreateOrder";
 
 const router = createBrowserRouter([
   {
@@ -27,15 +29,16 @@ const router = createBrowserRouter([
         element: <Cart />,
       },
       {
+        path: "/order/new",
+        element: <CreateOrder />,
+        action: createOrderAction,
+      },
+      {
         path: "/order/:orderId",
         element: <Order />,
         loader: orderLoader,
         errorElement: <Error />,
       },
-      //   {
-      //     path: "/order/new",
-      //     element: <CreateOrder />,
-      //   },
     ],
   },
 ]);
