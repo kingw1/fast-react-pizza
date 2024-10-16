fb4480bf34c330c52fdbc72f21a8061deea8e27e
King <wiwichai@gmail.com>
2024-10-16 20:01:42 +0700

    fetcher update order priority

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -8,6 +8,7 @@ import {
 import { getOrder } from "../../services/apiRestaurant";
 import { useFetcher, useLoaderData } from "react-router-dom";
 import { useEffect } from "react";
+import UpdateOrder from "./UpdateOrder";
 
 function Order() {
   const order = useLoaderData();
@@ -88,6 +89,8 @@ function Order() {
           To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}
         </p>
       </div>
+
+      {!priority && <UpdateOrder />}
     </div>
   );
 }
